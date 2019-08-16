4f9d6453cd485226a29b3342ae9ee63fef7c6910
Pete <vzp7444@autuni.ac.nz>
2019-08-16 13:30:19 +1200

    Added clearScreen() function
    No Javadoc comments yet - endure that it works first

--- src/SudokuRenderer/ViewRenderer.java
+++ src/SudokuRenderer/ViewRenderer.java
@@ -79,4 +79,9 @@ public class ViewRenderer
             System.out.println(line);
         }
     }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 }
