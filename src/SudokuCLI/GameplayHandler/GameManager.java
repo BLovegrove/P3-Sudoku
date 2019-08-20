7ecd5b28dec53fe398ee310d016d4d030f904b62
Brandon <b.lovegrove.wsd@gmail.com>
2019-08-20 15:50:38 +1200

    started game manager

---
+++ src/SudokuCLI/GameplayHandler/GameManager.java
@@ -0,0 +1,11 @@
+package SudokuCLI.GameplayHandler;
+
+import SudokuRenderer.ViewRenderer;
+
+public class GameManager
+{
+    public boolean newGame(ViewRenderer view, DifficultyLevel difficulty)
+    {
+        
+    }
+}
