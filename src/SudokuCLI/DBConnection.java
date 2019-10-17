fb93ba660e63f060cb90406fc559cc03d321ed13
Pete <vzp7444@autuni.ac.nz>
2019-10-17 20:53:33 +1300

    added db element
    need to add all functions

---
+++ src/SudokuCLI/DBConnection.java
@@ -0,0 +1,36 @@
+package SudokuCLI;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBConnection
+{
+    private static final String JDBC_URL = "jdbc:derby:P3-Sudoku;create=true";
+    private static final String username = "sudoku";
+    private static final String password = "pdc";
+    Connection conn;
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
