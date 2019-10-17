28163e947810fc64573905d4961b8fcff170d3ba
Pete <vzp7444@autuni.ac.nz>
2019-10-17 21:00:06 +1300

    added existing table check

--- src/SudokuCLI/DBConnection.java
+++ src/SudokuCLI/DBConnection.java
@@ -16,7 +16,43 @@ public class DBConnection
     private static final String password = "pdc";
     Connection conn;
 
-    
+    public void connectDB(){
+        try{
+            this.conn = DriverManager.getConnection(this.JDBC_URL, this.username, this.password);
+            System.out.println("Connected.");
+
+        } catch (SQLException ex){
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void createHighScoreTable(){
+        this.existingTableCheck("HIGHSCORE");
+
+    }
+
+    public void existingTableCheck(String name) {
+    try {
+        DatabaseMetaData dbmd = this.conn.getMetaData();
+        String[] types = {"TABLE"};
+        ResultSet rs = dbmd.getTables(null, null, null, types);
+        Statement statement = this.conn.createStatement();
+        while (rs.next()) {
+            String table_name = rs.getString("TABLE_NAME");
+            System.out.println(table_name);
+            if (table_name.equalsIgnoreCase(name)) {
+                statement.executeUpdate("Drop table " + name);
+                System.out.println("Table " + name + " has been deleted.");
+                break;
+            }
+        }
+        rs.close();
+        statement.close();
+    } catch (SQLException ex) {
+        Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+    }
+}
+
 
 
 
